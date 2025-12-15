6321758cfe46fc42fde1f29b96f4615f55d6edd9
Tatiana Villamizar <tm8497077@gmail.com>
2025-12-15 01:53:24 -0500

    feat: :sparkles: crear modelo Pago

---
+++ src/main/java/com/tati/model/Pago.java
@@ -0,0 +1,53 @@
+package com.tati.model;
+
+import java.time.LocalDate;
+
+public class Pago {
+
+    private int id;
+    private int prestamoId;
+    private LocalDate fechaPago;
+    private double monto;
+
+    public Pago() {
+    }
+
+    public Pago(int id, int prestamoId, LocalDate fechaPago, double monto) {
+        this.id = id;
+        this.prestamoId = prestamoId;
+        this.fechaPago = fechaPago;
+        this.monto = monto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrestamoId() {
+        return prestamoId;
+    }
+
+    public void setPrestamoId(int prestamoId) {
+        this.prestamoId = prestamoId;
+    }
+
+    public LocalDate getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDate fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+}
